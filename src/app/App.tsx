e35884e87abe6b21800cf027da1302e1a9d096bf
rjadyshkom <sergey@rjadysh.com>
2022-09-13 18:12:12 +0300

    fsd methodology

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import "./styles/index.scss";
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -15,8 +15,8 @@ export const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
