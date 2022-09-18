2e1793c09b026a596d27ff95c9d56d00ad446c7c
rjadyshkom <sergey@rjadysh.com>
2022-09-18 22:57:47 +0300

    first ui kit component

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,9 @@
 type Mods = Record<string, boolean | string>;
 
-const obj: Mods = {}
-
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             // исключаем из массива все false
