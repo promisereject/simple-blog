fd82d0f9b62c09dbb589f0ab7851d7c116871dcd
rjadyshkom <sergey@rjadysh.com>
2022-11-20 13:19:49 +0300

    pre-production commit

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CopyPlugin from 'copy-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -24,6 +25,11 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
             __API_URL__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
         }),
     ];
 
--- src/pages/ArticleEditPage/ui/ArticleEditPage.async.tsx
+++
@@ -1,6 +0,0 @@
-import { lazy } from 'react';
-
-export const ArticleEditPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleEditPage')), 400);
-}));
